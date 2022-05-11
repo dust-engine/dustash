c797249e0f3388916aca6b20f449a25140904fca
Zhixing Zhang <me@neoto.xin>
2022-05-11 02:26:49 -0700

    upgrade ash

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -107,10 +107,10 @@ impl Drop for DescriptorSet {
         let raw_pool = self.pool.raw.lock().unwrap();
         tracing::info!(device = ?raw_pool.0, "free descriptor set");
         unsafe {
-            self.pool
+            (self.pool
                 .device
                 .fp_v1_0()
-                .free_descriptor_sets(self.pool.device.handle(), raw_pool.0, 1, &self.raw)
+                .free_descriptor_sets)(self.pool.device.handle(), raw_pool.0, 1, &self.raw)
                 .result()
                 .unwrap();
         }
